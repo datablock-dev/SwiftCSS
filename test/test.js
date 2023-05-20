a5ddea719eaf03748ea7e43a4e8b5a9911626947
Patrick Tannoury <patrick.tannoury@klarna.com>
2023-05-20 18:07:39 +0200

    first commit

---
+++ test/test.js
@@ -0,0 +1,10 @@
+<div 
+    className="fs-22 fw-400 text-center user-select-none"
+    style-dark="test"
+    style-light=""
+    style-hover="cursor-pointer"
+>
+    <span>
+
+    </span>
+</div>
---
+++ test/x/test.jsx
@@ -0,0 +1,3 @@
+<div className="fw-600 white-space-normal">
+
+</div>
