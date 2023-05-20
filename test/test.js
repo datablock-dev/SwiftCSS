ff733e953b3f06566b827611fda17e157e72d454
Patrick Tannoury <patrick.tannoury@klarna.com>
2023-05-20 19:22:25 +0200

    added postcss and cssnano

--- test/test.js
+++ test/test.js
@@ -1,10 +1,10 @@
 <div 
-    className="fs-42 fw-200 text-center user-select-none brd-radius-6"
+    className="fs-42 fw-600 text-center user-select-none brd-radius-6"
     style-dark="test"
     style-light=""
     style-hover="cursor-pointer"
 >
-    <span>
+    <span className="z-300">
 
     </span>
 </div>
--- test/x/test.jsx
+++ test/x/test.jsx
@@ -1,4 +1,4 @@
-<div className="fw-400 white-space-normal brd-width-4 flex">
+<div className="fw-300 white-space-normal brd-width-4 flex">
     <div className="absolute hidden">
 
     </div>
